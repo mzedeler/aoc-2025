3115fdfad7a3c3ff0a4534f0a067d626e9ebca2b
michael@zedeler.dk <michael@zedeler.dk>
2025-12-03 22:51:10 +0100

    Second part.

---
+++ day-2/2.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node 
+
+const re = /^(\d+)\1+$/
+
+console.log(
+  require("fs")
+    .readFileSync(0)
+    .toString()
+    .split(/,/)
+    .filter(r => r)
+    .map(range => range.split(/-/).map(Number))
+    .flatMap(([start, end]) => {
+      const result = []
+      for (let i = start; i <= end; i++) {
+        result.push(i)
+      }
+      return result
+    })
+    .filter(id => re.exec(id))
+    .reduce((acc, id) => acc + id, 0)
+)
