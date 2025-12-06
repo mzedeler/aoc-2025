929d30c3b619dd1862d9041158196c291ca86e4e
michael@zedeler.dk <michael@zedeler.dk>
2025-12-06 23:00:31 +0100

    Cleanup of first part for day 4.

--- day-4/1.js
+++ day-4/1.js
@@ -15,21 +15,23 @@ const offsets = [
   [-1, 1],  [0, 1],  [1, 1],
 ]
 
-const result = []
-let count = 0
-for (let y = 0; y < map.length; y++) {
-  result.push([])
-  for (let x = 0; x < map[y].length; x++) {
-    if (map[y][x] !== '@') {
-      result[y].push(0)
-      continue
+const getRemovable = (state) => {
+  const removable = []
+  for (let y = 0; y < state.length; y++) {
+    removable.push([])
+    for (let x = 0; x < state[y].length; x++) {
+      if (state[y][x] !== '@') {
+        removable[y].push(0)
+        continue
+      }
+      let count = 0
+      for (let [dx, dy] of offsets) {
+        count += peek(x + dx, y + dy)
+      }
+      removable[y].push(count > 3 ? 0 : 1)
     }
-    let count = 0
-    for (let [dx, dy] of offsets) {
-      count += peek(x + dx, y + dy)
-    }
-    result[y].push(count > 3 ? 0 : 1)
   }
+  return removable
 }
 
-console.log(result.flat().reduce((a, v) => a + v, 0))
+console.log(getRemovable(map).flat().reduce((a, v) => a + v, 0))
